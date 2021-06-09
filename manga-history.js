db1dc8cf88fac8d250968817eb03988df2da111f
nguyenluongtoan <toanmta1990@gmail.com>
2021-06-10 06:02:10 +0700

    them 1 so truyen lich su

--- manga-history.js
+++ manga-history.js
@@ -3,3 +3,8 @@ lua tinh, trom vat, tai nan giao thong
 https://qmanga.net/chi-ve-su-van-dong-cua-dia-cau/chuong-2_463903.html
 chi-ve-su-van-dong-cua-dia-cau
 https://qmanga.net/the-man-who-killed-nobunaga.html
+https://qmanga.net/sangokushi/chuong-1_393546.html 3 quoc by japan
+https://qmanga.net/binh-phap-mac-cong.html 
+https://qmanga.net/zipang/chuong-1_64647.html Vào năm 200x, khu trục hạm Mirai tối tân thuộc Lực lượng Phòng vệ Biển Nhật Bản đã ra khơi trong một nhóm 4 tàu để tham gia tập trận cùng Hải quân Hoa Kỳ.
+https://qmanga.net/conquests-chien-chinh.html
+https://qmanga.net/conde-koma/chuong-1_339156.html
