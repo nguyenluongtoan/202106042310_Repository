cf500eba52c68a709f4286b1573a27a0aaf2857a
nguyenluongtoan <toanmta1990@gmail.com>
2021-06-11 03:16:25 +0700

    Update viblo-java-multithread.java

--- viblo-java-multithread.java
+++ viblo-java-multithread.java
@@ -1,1 +1,16 @@
 https://viblo.asia/p/001-parallel-computing-hardware-p1-Ljy5Vvvy5ra
+1. 001: Parallel computing hardware P1
+2. 002: Parallel computing hardware P2
+3. 003: Thread và Process
+4. 004: Concurrent và parallel
+5. 005: OS đối xử với thread thế nào?
+6. 006: Vòng đời của một thread diễn ra như thế nào trong Java?
+7. 007: Data race và Mutual exclusion
+8. 008: Lock và lock-free trong Java, từ lý thuyết đến thực tiễn
+9. 009: Deadlock, Livelock, Abandoned lock và Starvation
+10. 010: Nhầm tưởng về Data race và Race condition?
+11. 011: Cách giải quyết Race condition
+12. 012: Sự thật về Hyper-threading
+13. 013: Lập trình multi-thread có thật sự nhanh hơn single-thead?
+14. 014: Đánh giá performance khi lập trình multi-thread P1
+15. 015: Đánh giá performance khi lập trình multi-thread P2
