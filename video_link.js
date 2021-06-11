e5fae0dc0202c4bf4e37f518254917c8be2aa314
nguyenluongtoan <toanmta1990@gmail.com>
2021-06-11 22:08:00 +0700

    Update video_link.js

--- video_link.js
+++ video_link.js
@@ -1,1 +1,2 @@
 https://www.youtube.com/watch?v=i8z1ZhfrbM0
+https://www.facebook.com/watch/?ref=saved&v=1008002386283050 Hướng Dẫn Ghi Nhớ Nhanh 2136 Kanji Từ N5 Đến N1 Theo Câu Chuyện Và Chữ Kanji Giống Nhau!
