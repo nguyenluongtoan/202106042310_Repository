0042e7a590761401e039a5968d6d16a39d2b9e94
nguyenluongtoan <toanmta1990@gmail.com>
2021-06-26 06:56:20 +0700

    Update manga-history.js

--- manga-history.js
+++ manga-history.js
@@ -1,6 +1,6 @@
 http://www.nettruyentop.com/truyen-tranh/tham-tu-conan/chap-1068/682099
 lua tinh, trom vat, tai nan giao thong
-https://qmanga.net/chi-ve-su-van-dong-cua-dia-cau/chuong-2_463903.html
+http://www.nettruyentop.com/truyen-tranh/chi-ve-su-van-dong-cua-dia-cau/chap-14/726753
 chi-ve-su-van-dong-cua-dia-cau
 https://qmanga.net/the-man-who-killed-nobunaga.html
 https://qmanga.net/sangokushi/chuong-1_393546.html 3 quoc by japan
