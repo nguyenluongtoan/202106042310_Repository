fbf3b3e4dd6e36185e6998a08081fd8ddf681e61
nguyenluongtoan <toanmta1990@gmail.com>
2021-06-26 08:38:02 +0700

    Update manga-history.js

--- manga-history.js
+++ manga-history.js
@@ -1,10 +1,16 @@
-http://www.nettruyentop.com/truyen-tranh/tham-tu-conan/chap-1068/682099
-lua tinh, trom vat, tai nan giao thong
-http://www.nettruyentop.com/truyen-tranh/chi-ve-su-van-dong-cua-dia-cau/chap-14/726753
-chi-ve-su-van-dong-cua-dia-cau
+http://www.nettruyentop.com/truyen-tranh/tham-tu-conan/chap-1068/682099   lua tinh, trom vat, tai nan giao thong
+http://www.nettruyentop.com/truyen-tranh/chi-ve-su-van-dong-cua-dia-cau/chap-14/726753    chi-ve-su-van-dong-cua-dia-cau
 https://qmanga.net/the-man-who-killed-nobunaga.html
 https://qmanga.net/sangokushi/chuong-1_393546.html 3 quoc by japan
 https://qmanga.net/binh-phap-mac-cong.html 
 https://qmanga.net/zipang/chuong-1_64647.html Vào năm 200x, khu trục hạm Mirai tối tân thuộc Lực lượng Phòng vệ Biển Nhật Bản đã ra khơi trong một nhóm 4 tàu để tham gia tập trận cùng Hải quân Hoa Kỳ.
 https://qmanga.net/conquests-chien-chinh.html
 https://qmanga.net/conde-koma/chuong-1_339156.html
+http://truyenqq.com/truyen-tranh/doi-quan-dat-nung-7228-chap-3.html
+http://truyenqq.com/truyen-tranh/judas-chuyen-ke-toi-do-4806
+http://truyenqq.com/truyen-tranh/rainbow-3105
+http://truyenqq.com/truyen-tranh/the-legend-of-wonder-woman-1808
+http://truyenqq.com/truyen-tranh/lucky-luke-1470
+http://truyenqq.com/truyen-tranh/son-tac-vuong-seyu-1181-chap-1.html
+http://truyenqq.com/truyen-tranh/li-thuan-thuan-chien-binh-va-ho-the-gia-1420-chap-1.html
+http://truyenqq.com/truyen-tranh/xuan-thu-chien-hung-249-chap-6.html
